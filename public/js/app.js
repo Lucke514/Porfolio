cadb75bc0bab049cf8b0b7315ba94f89f4612c18
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-02-04 15:53:31 -0300

    front and validacion upgrade

--- public/js/app.js
+++ public/js/app.js
@@ -7,10 +7,28 @@ menuItems.style.display = 'none';
 
 
 //  --> Eventos
+
+// -> Header
 menuButton.addEventListener('click', () => {
     menuItems.style.display = menuItems.style.display === 'none' ? 'block' : 'none';
 });
 
-menuButton.addEventListener('blur', () => {
+menuButton.addEventListener('focusout', () => {
     menuItems.style.display = 'none';
 });
+
+menuItems.addEventListener('click', () => {
+    menuItems.style.display = 'none';
+});
+
+document.addEventListener('click', function(e) {
+    const targetElement = e.target; 
+    const menuIsVisible = menuItems.style.display === 'block';
+
+    // Verifica si el clic fue fuera del menú y del botón que lo controla, y si el menú está visible
+    if (menuIsVisible && !menuButton.contains(targetElement) && !menuItems.contains(targetElement)) {
+        menuItems.style.display = 'none';
+    }
+});
+
+// -> Footer
